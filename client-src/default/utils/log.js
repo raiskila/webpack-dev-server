eb88ad9f7de03b97bc2ae582a934d3a6e5686667
Janne Raiskila <janne@raiskila.com>
2020-11-28 19:10:12 +0200

    fix log level

--- client-src/default/utils/log.js
+++ client-src/default/utils/log.js
@@ -5,7 +5,7 @@ const log = require('../../transpiled-modules/log');
 const name = 'webpack-dev-server';
 // default level is set on the client side, so it does not need
 // to be set by the CLI or API
-const defaultLevel = 'info';
+const defaultLevel = 'warn';
 
 function setLogLevel(level) {
   log.configureDefaultLogger({
